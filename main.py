326ac15806b74b227e848b2be5a2f641c4c064d7
Ryan Tseng <ryantseng123@mail.edu.tw>
2026-04-18 15:40:47 +0800

    feat: Added CORS Policy

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # customize this to only allow certain domains!
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/hello")
 def hello():
     return {"message": "hello"}
