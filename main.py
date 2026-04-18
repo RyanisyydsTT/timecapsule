00a2a15d354fbd7158b99a6430eb1137a2220ac9
Ryan Tseng <ryantseng123@mail.edu.tw>
2026-04-18 15:53:27 +0800

    feat: DB code initial

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+import sqlite3 
+
+DB_PATH = "capsule.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS capsules (
+    id TEXT PRIMARY KEY,
+    content TEXT NOT NULL,
+    create_at TEXT NOT NULL,
+    unlock_at TEXT NOT NULL,
+    )
+                   """)
+
+def get_db()
+           return sqlite3.connect(DB_PATH)
 
 app = FastAPI()
 
@@ -11,7 +29,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/hello")
+@app.on_event("statup")
+def startup():
+    init_db()
+
+@app.get("/store")
 def hello():
     return {"message": "hello"}
 
